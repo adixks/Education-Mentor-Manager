2ba78743f9512246380060f9a38dac4c67046104
adixks <adrianks@op.pl>
2023-11-06 01:24:27 +0100

    Add pom and basic class

---
+++ src/main/java/pl/szlify/codingapi/CodingApiApplication.java
@@ -0,0 +1,13 @@
+package pl.szlify.codingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingApiApplication.class, args);
+	}
+
+}
