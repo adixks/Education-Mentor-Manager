6fd79d2969a7cb037a3262564ece359c2d97073f
adixks <adrianks@op.pl>
2023-12-01 18:59:22 +0100

    Small fixes

--- src/main/java/pl/szlify/codingapi/strategy/ListStrategy.java
+++
@@ -1,8 +0,0 @@
-package pl.szlify.codingapi.strategy;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface ListStrategy<T> {
-    Page<T> getList(Pageable pageable);
-}
