adbdd09a548f13c90a54b078cd2529d3e5cb76b1
adixks <adrianks@op.pl>
2023-12-07 14:04:52 +0100

    Small fix

--- src/test/java/pl/szlify/codingapi/service/IntegrationTest.java
+++ src/test/java/pl/szlify/codingapi/service/IntegrationTest.java
@@ -40,7 +40,7 @@ import pl.szlify.codingapi.repository.TeacherRepository;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@TestPropertySource(locations="classpath:application-dev.properties")
+@TestPropertySource(locations="classpath:application-prod.properties")
 @AutoConfigureMockMvc
 @Transactional
 //@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:beforeTestRun.sql")
